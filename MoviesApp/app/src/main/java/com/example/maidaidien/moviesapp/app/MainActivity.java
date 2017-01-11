d75005a97c8dfbfbb3243d8bed8fdbc9fac4c2e7
luckyluke1994 <mddien1994@gmail.com>
2017-01-11 13:53:11 +0700

    Add menu setting

--- MoviesApp/app/src/main/java/com/example/maidaidien/moviesapp/app/MainActivity.java
+++ MoviesApp/app/src/main/java/com/example/maidaidien/moviesapp/app/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.maidaidien.moviesapp.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -14,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
                 .add(R.id.container, new MoviesFragment())
                 .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
